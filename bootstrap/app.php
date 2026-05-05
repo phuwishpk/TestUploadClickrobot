03ef31afc9dc7c9d8708da7bbfe59219067c5178
phuwishpk <phuwish123@gmail.com>
2026-05-06 03:51:32 +0700

    Add CSRF token validation middleware and remove sessions table migration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,6 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \Illuminate\Foundation\Http\Middleware\ValidateCsrfToken::class,
         ]);
 
         $middleware->alias([
