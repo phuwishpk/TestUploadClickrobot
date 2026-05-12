b822a4a914389a3a0972d2f5b69296d4fa890788
phuwishpk <phuwish123@gmail.com>
2026-05-12 11:18:28 +0700

    Refactor student code generation chage to call and build name

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -47,40 +47,13 @@ class Student extends Model
         return $this->hasMany(Media::class);
     }
 
-    public static function generateCode(int $classroomId = null): string
-    {
-        $query = self::query();
-
-        if ($classroomId) {
-            $query->where('classroom_id', $classroomId);
-        }
-
-        $lastStudent = $query->orderBy('id', 'desc')->first();
-        $nextNumber = 1;
-
-        if ($lastStudent) {
-            preg_match('/\d+/', $lastStudent->code, $matches);
-            $nextNumber = $matches ? ((int) $matches[0] + 1) : 1;
-        }
-
-        // Skip codes already taken by a user account (orphaned from deleted students)
-        do {
-            $code = 'นร' . str_pad($nextNumber, 3, '0', STR_PAD_LEFT);
-            $taken = User::on('mysql')->where('student_code', $code)->exists()
-                  || self::where('code', $code)->exists();
-            $nextNumber++;
-        } while ($taken);
-
-        return $code;
-    }
-
     public static function boot()
     {
         parent::boot();
 
-        static::creating(function ($student) {
+        static::created(function ($student) {
             if (empty($student->code)) {
-                $student->code = self::generateCode($student->classroom_id);
+                $student->update(['code' => $student->id . '_' . $student->name]);
             }
         });
     }
